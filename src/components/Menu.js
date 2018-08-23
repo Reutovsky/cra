b42d2389a46904955233488b5c5c3eb0f86ab664
Dmitry Ivanov <Webdev.Dmitry@gmail.com>
2018-08-23 17:48:46 +0300

    create class with proptypes and defaultProps

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,41 @@
 import React from 'react'
 import Recipe from './Recipe'
+import PropTypes from 'prop-types'
+
+class Summary extends React.Component {
+
+	static propTypes = {
+		title: PropTypes.string,
+		ingredients: PropTypes.array,
+		steps: PropTypes.array
+	}
+
+    static defaultProps = {
+		title: "defaulttitle",
+		ingredients: "defaultingridients",
+		steps: "defaultsteps" 
+	}
+	
+	render() {
+		const {title, ingredients, steps} = this.props
+		return (
+			<div>
+				<h1>{title} - title</h1>
+				<p>{ingredients.length} Ingreds | {steps.length} Steps</p>
+			</div>
+		)
+	}
+}
+
+const summarydata = {
+	title: "Некий Тайтлы",
+	ingredients: "peanut butter, jelly, bread",
+	steps: "one two three" 
+}
 
 const Menu = ({ recipes }) =>
 	<article>
+		<Summary {...summarydata} />
 		<header>
 			<h1>Delicious Recipes</h1>
 		</header>
@@ -13,6 +46,5 @@ const Menu = ({ recipes }) =>
 		</div>
 	</article>
 
-	
 
 export default Menu
